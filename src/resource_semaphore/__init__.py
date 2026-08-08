74127603e822c7a1b55caea49f1c30df2582348b
Abdulrahman Hedya <abdohedya19@gmail.com>
2026-08-08 13:39:11 +0300

    chore: bump version

--- src/resource_semaphore/__init__.py
+++ src/resource_semaphore/__init__.py
@@ -1,3 +1,10 @@
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("resource-semaphore")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "1.0.0"
+
 from .asynchronous import AsyncNoopResourceSemaphore, AsyncResourceSemaphore
 from .synchronous import NoopResourceSemaphore, ResourceSemaphore
 
@@ -6,4 +13,5 @@ __all__ = [
     "NoopResourceSemaphore",
     "AsyncNoopResourceSemaphore",
     "AsyncResourceSemaphore",
+    "__version__",
 ]
